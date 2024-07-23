1bdb727f8093fb85af525f25a62f008dcb3b7e30
Ankit <ankit.sharma@ksolves.com>
2024-07-23 18:38:55 +0530

    new

---
+++ tests/test.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+
+  await page.goto("https://shakawear-staging-wt-validate-cron-14252838.dev.odoo.com/web/login");
+
+  await page.fill('input[placeholder="Email"]', "anthony@shakawear.com");
+  await page.fill('input[placeholder="Password"]', "123");
+  await page.click('button:text("Log in")');
+
+  await page.click('a[data-menu-xmlid="sale.sale_menu_root"]');
+  await page.click('button:text("Create")');
+
+  await page.fill('input[class="o_input ui-autocomplete-input"]',"ALPHABRODER");
+  await page.click('text="ALPHABRODER"');
+
+  await page.click('a:has-text("Add a product")');
+  await page.fill('div[name="product_id"] input[type="text"]', "302001XL");
+  await page.click('text="[302001XL] Classic Deep V-Neck White Xl"');
+
+  await page.click('button:text("Save")');
+  await page.click('button:text("Confirm")');
+//   await page.click('button[name="action_confirm"]');
+});
+
